923400f54a27cc91878a0b1014f256c1006910b4
KevinFritsch <39443921+KevinFritsch@users.noreply.github.com>
2020-01-20 22:02:54 +0100

    Update databaseConfig.php

--- connection/databaseConfig.php
+++ connection/databaseConfig.php
@@ -1,7 +1,7 @@
 <?php
 const SQL_DSN = 'mysql:host=db778611904.hosting-data.io;dbname=db778611904';
-const SQL_USERNAME = 'dbo778611904';
-const SQL_PASSWORD = 'projectManager11.';
+const SQL_USERNAME = 'USERNAME';
+const SQL_PASSWORD = 'PASSWORD';
 
 try{
 	$db = new PDO(SQL_DSN, SQL_USERNAME, SQL_PASSWORD);
